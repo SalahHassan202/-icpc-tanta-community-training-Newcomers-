cc8c0f8e051b4fe07f10d993c34f6d97e34a4bf0
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-03-25 23:50:40 +0200

    Week(1)

---
+++ Week 1/D - Simple Calculator.cpp
@@ -0,0 +1,17 @@
+// D - Simple Calculator
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+#include<algorithm>
+#include<string>
+using namespace std;
+int  main()
+{
+	long long a , b ;         cin>>a >> b ;
+	cout<<a<<" + "<<b <<" = " <<a+b<<endl;
+	cout<<a<<" * "<<b <<" = " <<a*b<<endl;
+	cout<<a<<" - "<<b <<" = " <<a-b<<endl;
+
+	return 0;
+}
+	
