1e3b9764a03dc1f448f111339c68fa5defa66d41
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-03-25 23:54:14 +0200

    Week(1)

---
+++ Week 1/L - Difference.cpp
@@ -0,0 +1,16 @@
+// L - Difference
+#include<iostream>
+#include<cmath>
+#include<algorithm>
+#include<iomanip>
+#include<string>
+using namespace std;
+int  main()
+{
+	long long a , b , c , d ;        
+	cin>>a >> b >> c >>d ;
+	cout<<"Difference"<<" = "<< (a * b )- (c * d )<<endl;
+
+	return 0;
+}
+	
