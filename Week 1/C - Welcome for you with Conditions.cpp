36a79361e4252453c5527245f050e67f16f98a57
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-03-25 23:50:14 +0200

    Week(1)

---
+++ Week 1/C - Welcome for you with Conditions.cpp
@@ -0,0 +1,23 @@
+// C - Welcome for you with Conditions
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+#include<algorithm>
+#include<string>
+using namespace std;
+int  main()
+{
+	int a , b ;
+	cin>>a>>b ;
+	if(a>= b)
+	{
+		cout<<"Yes"<<endl;
+	} 
+	else
+	{
+		cout<<"No"<<endl;
+	}
+
+	return 0;
+}
+	
