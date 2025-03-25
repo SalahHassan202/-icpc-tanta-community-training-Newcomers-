0f4a749c1aff7b95949b2843f85e575a080eaf08
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-03-25 23:52:57 +0200

    Week(1)

---
+++ Week 1/I - Simple Remainder.cpp
@@ -0,0 +1,17 @@
+// I - Simple Remainder
+#include<iostream>
+#include<cmath>
+#include<algorithm>
+#include<iomanip>
+#include<string>
+using namespace std;
+int  main()
+{
+	int x , y  ;   
+	cin>>x>>y ;
+	
+       cout<<x%y<<endl;
+
+ 	return 0;
+}
+	
