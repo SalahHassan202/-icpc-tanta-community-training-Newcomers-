6d128755cd70449e34c5843aa21c320d1fed855d
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-03-27 23:15:43 +0200

    Week(2)

---
+++ Week 2/A - 1 to N .cpp
@@ -0,0 +1,16 @@
+// A - 1 to N 
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main()
+{
+	int num ;       cin >> num ;
+	for(int i = 1 ; i<= num ; i++)
+	{
+		cout<< i <<endl;
+	}
+
+
+	return 0;
+}
