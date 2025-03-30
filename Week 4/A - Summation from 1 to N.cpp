fe328903870484514f6bea859563e80b0ec14a03
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-03-30 16:57:07 +0200

    Week(4)

---
+++ Week 4/A - Summation from 1 to N.cpp
@@ -0,0 +1,15 @@
+// A - Summation from 1 to N
+#include <iostream>
+# include <string >
+#include <algorithm>
+using namespace std;
+int main ()
+{
+    long long num ;        cin >> num ;
+    long long sum = 0 ; 
+    sum = num * (num + 1) / 2 ;
+    cout<< sum <<endl; 
+
+  
+    return 0;
+}
