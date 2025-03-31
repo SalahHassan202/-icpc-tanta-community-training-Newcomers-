4e572ab162bebd7af5e804056963e1345f502f0b
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-03-31 18:39:11 +0200

    Week(5)

---
+++ Week 5/M - I miss you....cpp
@@ -0,0 +1,18 @@
+// M - I miss you...
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    string s1 ;               cin >> s1 ;
+    for(int i =0 ; i < s1.size() ; i++)
+    {
+        // gone ====>  size = 4     0 1 2 3  
+ 
+        cout<<"x";
+    }
+
+    
+    return 0;
+}
